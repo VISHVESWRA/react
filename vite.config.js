d7d5f299c4707548f9a81bb9f143e4a61f6ddc7a
VISHVESWRA <vishveswravichu@gmail.com>
2025-08-12 17:39:29 +0530

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+const isProd = process.env.NODE_ENV === 'production';
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/react/',
+  base: isProd ?  '/react/' : '/',
   plugins: [react()],
 })
