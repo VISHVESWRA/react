c7c106d8185ed5f60b58eb3b3e9fc92882f1071d
VISHVESWRA <vishveswravichu@gmail.com>
2025-08-19 10:23:49 +0530

    fix: login

--- src/api/Student.js
+++ src/api/Student.js
@@ -1,12 +1,17 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-export const fetchStudent = createAsyncThunk('students/fetchStudent', async () => {
+export const fetchStudent = createAsyncThunk(
+  "students/fetchStudent",
+  async () => {
     const list = await fetch(`http://localhost:9000/students`);
     const data = await list?.json();
     return data;
-})
+  }
+);
 
-export const postData = createAsyncThunk('students/postData', async (newData) => {
+export const postData = createAsyncThunk(
+  "students/postData",
+  async (newData) => {
     // const list = await fetch('http://localhost:9000/students/add', {
     //     method: "POST",
     //     headers: {
@@ -18,14 +23,30 @@ export const postData = createAsyncThunk('students/postData', async (newData) =>
     // alert('Student added')
     // return data
 
-    const query = JSON.stringify(newData)
+    const query = JSON.stringify(newData);
 
     const list = await fetch(`http://localhost:9000/students/add?data=${query}`)
-                            .then(response => response.json())
-                            .then(data=> alert(data.error))
-                            .catch(e => {
-                                alert(e)
-                            })
+      .then((response) => response.json())
+      .then((data) => alert(data.error))
+      .catch((e) => {
+        alert(e);
+      });
     const data = await list?.json();
     return data;
-})
+  }
+);
+
+export const signin = createAsyncThunk("students/signIn", async (data) => {
+  const query = JSON.stringify(data);
+
+  const response = await fetch(
+    `http://localhost:9000/students/signIn?data=${query}`
+  )
+    .then((res) => res.json())
+    .catch((e) => {
+      alert(e);
+    });
+
+  const getting = await response?.json();
+  return getting;
+});
--- src/student/StudentList.jsx
+++ src/student/StudentList.jsx
@@ -1,44 +1,67 @@
 import { useDispatch } from "react-redux";
-import { fetchStudent, postData } from "../api/Student";
-import './StudentList.css'
+import { fetchStudent, postData, signin } from "../api/Student";
+import "./StudentList.css";
 import { useForm } from "react-hook-form";
 
 function StudentList() {
+  const dispatch = useDispatch();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-    const dispatch = useDispatch();
-    const { register, handleSubmit, formState: errors } = useForm();
+  const signUp = (data) => {
+    console.log(data);
+    dispatch(postData(data));
+  };
 
-    const newData = {
-        "name": "Anito",
-        "id": "S004",
-        "age": 21,
-        "password": "pass123"
-    }
+  const signIn = (data) => {
+    console.log(data);
+    dispatch(signin(data));
+  };
 
-    const submit = (data) => {
-        console.log(data);
-        dispatch(postData(data))
-    }
-
-    return (
+  return (
+    <>
+      <h2>User Login</h2>
+      <form>
         <>
-            <h2>User Login</h2>
-            <form onSubmit={handleSubmit(submit)}>
-                <input type="text" id="name" placeholder="Name" {...register('name', { required: 'Required' })} />
-                {errors.name && <p>{errors.name.message}</p>}
-                <input type="text" id="id" placeholder="User ID"  {...register('id')} />
-                <input type="number" id="age" placeholder="Age"  {...register('age')} />
-                <input type="password" id="password" placeholder="Password"  {...register('password')} />
-                <button type="submit">Login</button>
-            </form>
-
-            <div className="message" id="message"></div>
-
-            <h1>Student List</h1>
-            <button onClick={() => dispatch(fetchStudent())}>Fetch</button>
-            <button onClick={() => dispatch(postData(newData))}>Post</button>
+          <input
+            type="text"
+            id="name"
+            placeholder="Name"
+            {...register("name", { required: "Required" })}
+          />
+          {errors.name && <p>{errors.name.message}</p>}
         </>
-    )
+        <input type="text" id="id" placeholder="User ID" {...register("id")} />
+        <input type="number" id="age" placeholder="Age" {...register("age")} />
+        <>
+          <input
+            type="password"
+            id="password"
+            placeholder="Password"
+            {...register("password", { required: "Required" })}
+          />
+          {errors.password && <p>{errors.password.message}</p>}
+        </>
+        <div style={{ display: "flex", flexDirection: "row", gap: "10px" }}>
+          <button type="submit" onClick={handleSubmit(signUp)}>
+            Sign Up
+          </button>
+          <button type="submit" onClick={handleSubmit(signIn)}>
+            Login
+          </button>
+        </div>
+      </form>
+
+      <div className="message" id="message"></div>
+
+      <h1>Student List</h1>
+      <button onClick={() => dispatch(fetchStudent())}>Fetch</button>
+      {/* <button onClick={() => dispatch(postData(newData))}>Post</button> */}
+    </>
+  );
 }
 
 export default StudentList;
